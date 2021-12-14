bfd300d7ab5db8d24475760bc3f2d963d06d50d1
BA <39313572+eromose-le@users.noreply.github.com>
2021-12-14 11:30:14 +0100

    test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-    <h1>Netlify Application </h1>
+    <h1>Netlify Application II</h1>
     </div>
   );
 }
